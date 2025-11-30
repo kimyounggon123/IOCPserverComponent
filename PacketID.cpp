fb5dd5f703dff9975c54f4b13cc164ac5358fe10
kimyounggon123 <theno2727@gmail.com>
2025-11-30 13:56:25 +0900

    2025/11/30

--- PacketID.cpp
+++ PacketID.cpp
@@ -1,1 +1,11 @@
 #include "PacketID.h"
+int PacketType::count = 0;
+const int32_t PacketType::Default = PacketType::Base + PacketType::Next();
+const int32_t PacketType::ServerIsClosed = PacketType::Base + PacketType::Next();
+
+int PacketResult::count = 0;
+const int32_t PacketResult::Try = PacketResult::Base + PacketResult::Next();
+const int32_t PacketResult::WaitDatabase = PacketResult::Base + PacketResult::Next();
+const int32_t PacketResult::Success = PacketResult::Base + PacketResult::Next();
+const int32_t PacketResult::Fail = PacketResult::Base + PacketResult::Next();
+const int32_t PacketResult::BroadCast = PacketResult::Base + PacketResult::Next();
--- PacketID.h
+++ PacketID.h
@@ -7,18 +7,24 @@
 struct PacketType
 {
 	static constexpr int32_t Base = 0;
-	static constexpr int32_t Default = Base + 0;
-	static constexpr int32_t ServerIsClosed = Base + 1;
+	static int count;
+	static int Next() { return count++; }
+
+	static const int32_t Default;
+	static const int32_t ServerIsClosed;
 };
 
 struct PacketResult
 {
 	static constexpr int32_t Base = 0;
-	static constexpr int32_t Try = Base + 1;
-	static constexpr int32_t WaitDatabase = Base + 2;
-	static constexpr int32_t Success = Base + 3;
-	static constexpr int32_t Fail = Base + 4;
-	static constexpr int32_t BroadCast = Base + 5;
+	static int count;
+	static int Next() { return count++; }
+
+	static const int32_t Try;
+	static const int32_t WaitDatabase;
+	static const int32_t Success;
+	static const int32_t Fail;
+	static const int32_t BroadCast;
 };
 
 
